294e04384a75cd83ded496c130113a1135c1e38e
jackscotti <jacopojackscotti@gmail.com>
2015-12-30 23:38:03 +0100

    Show crime data

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# crimy
+# Crimy
+
+An app that will tell you the number and type of crimes near the house you've found on Zoopla or Rightmove.
 
 DONE:
 - pull out lat and long from Zoopla or Rightmove ads
+- take long and lat and find crimes through police API
+- show interesting crime data
 
 TODO:
-- take long and lat and find crimes through police API
+- hide errors, show an error page
+- make the app pretty
+- add tests
+- push to Heroku
--- lib/app.rb
+++ lib/app.rb
@@ -52,12 +52,37 @@ get '/' do
 end
 
 get '/crimes' do
-  lat = params['lat']
-  long = params['long']
+  @lat = params['lat']
+  @long = params['long']
 
-  unless lat && long
+  unless @lat && @long
     "you must provide <strong>lat</strong> and <strong>long</strong> in the body of your request"
   else
-    "<h1>Coordinates</h1>\n lat:" + lat + ", long:" + long
+    erb :crimes, locals: {
+      lat: @lat,
+      long: @long,
+      crimes_at_location: crimes_at_location,
+      crimes_1_mile_radious: crimes_1_mile_radious,
+    }
   end
+end
+
+def crimes_at_location
+  '''
+  crimes at location
+  https://data.police.uk/api/crimes-at-location?lat=#{lat}&lng=#{long}
+  '''
+  url = "https://data.police.uk/api/crimes-at-location?lat=#{@lat}&lng=#{@long}"
+  HTTParty.get(url)
+end
+def crimes_1_mile_radious
+  '''
+  street level crimes (1 mile radious)
+  "https://data.police.uk/api/crimes-street/all-crime?lat=#{lat}&lng=#{long}"
+  '''
+  url = "https://data.police.uk/api/crimes-street/all-crime?lat=#{@lat}&lng=#{@long}"
+  response = HTTParty.get(url)
+
+  groups = response.group_by {|x| "#{x['category']}"}
+  groups.map {|k,v| {"type" => k, "total" => v.count} }.sort_by {|x| x["total"]}.reverse
 end
